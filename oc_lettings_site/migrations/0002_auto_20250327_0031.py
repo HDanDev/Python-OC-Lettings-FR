b1d6139f2f5a2f051c931bfc03ab6f7c880a5cf8
Dan <72689919+HDanDev@users.noreply.github.com>
2025-03-28 13:46:38 +0100

    Added dependencies in data deletion migration to prevent failure

--- oc_lettings_site/migrations/0002_auto_20250327_0031.py
+++ oc_lettings_site/migrations/0002_auto_20250327_0031.py
@@ -7,6 +7,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('oc_lettings_site', '0001_initial'),
+        ('lettings', '0002_copy_data'),
+        ('profiles', '0002_copy_data'),
     ]
 
     operations = [
